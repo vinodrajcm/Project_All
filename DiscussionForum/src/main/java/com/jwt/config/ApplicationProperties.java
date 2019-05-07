8e8934001ef3342f2e6a7999c60536f80c408d90
vinodrajcm <vinodraj.muniraju@kennametal.com>
2019-05-07 14:09:32 +0530

    new password

--- DiscussionForum/src/main/java/com/jwt/config/ApplicationProperties.java
+++ DiscussionForum/src/main/java/com/jwt/config/ApplicationProperties.java
@@ -5,9 +5,9 @@ public class ApplicationProperties {
 	
 	public static final String XML = "application/xml";
 	
-	public static final String AUTH = "Basic dmVua2FtazpKYW4yMHRoQDIwMTk=";
+	public static final String AUTH = "Basic bXVuaXJ2YzpKYW4yNnRoQDEyMw==";
 	
 	public static final String USERID = "munirvc";
 	
-	public static final String PASSWORD = "Plmnb@12345";
+	public static final String PASSWORD = "Jan26th@123";
 }
